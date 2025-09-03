b3dd108846b154b7bdb51c87c726e359cdc99947
Veera <veera.heikkinen95@gmail.com>
2025-09-03 11:45:43 +0300

    Tehtävä 3 valmis

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
 
 
 export default function App() {
@@ -8,12 +8,20 @@ export default function App() {
   const [secondNumber, setSecondNumber] = useState();
   const [result, setResult] = useState();
 
-  function handleSum() {
-    setResult(firstNumber + secondNumber);
-  }
+  const [calculations, setCalculations] = useState([]);
 
-  function handleSubtraction() {
-    setResult(firstNumber - secondNumber);
+  function handleClick(operator) {
+
+    if (operator === "+") {
+      setResult(parseInt(firstNumber) + parseInt(secondNumber));
+      setCalculations([...calculations, firstNumber + " + " + secondNumber + " = " + (parseInt(firstNumber) + parseInt(secondNumber))]);
+    } else {
+      setResult(parseInt(firstNumber) - parseInt(secondNumber));
+      setCalculations([...calculations, firstNumber + " - " + secondNumber + " = " + (parseInt(firstNumber) - parseInt(secondNumber))]);
+    }
+
+    setFirstNumber("");
+    setSecondNumber("");
   }
 
   return (
@@ -22,26 +30,31 @@ export default function App() {
       <TextInput
         placeholder='Insert a number'
         keyboardType='number-pad'
-        onChangeText={number => setFirstNumber(parseInt(number))}
+        onChangeText={number => setFirstNumber(number)}
         value={firstNumber}
       />
       <TextInput
         placeholder='Insert a number'
         keyboardType='number-pad'
-        onChangeText={number => setSecondNumber(parseInt(number))}
+        onChangeText={number => setSecondNumber(number)}
         value={secondNumber}
       />
       <View style={styles.buttons}>
         <Button
           title='+'
-          onPress={handleSum}
+          onPress={() => handleClick("+")}
         />
         <Button
           title='-'
-          onPress={handleSubtraction}
+          onPress={() => handleClick("-")}
         />
 
       </View>
+      <Text>History: </Text>
+      <FlatList
+        data={calculations}
+        renderItem={({ item }) => <Text>{item}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -53,6 +66,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 100,
   },
   buttons: {
     flexDirection: "row"
